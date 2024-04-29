585c7257ec422e72179980e6393bbf1ec9211463
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-04-30 02:16:38 +0530

    Create MinXOR.cpp

---
+++ MinXOR.cpp
@@ -0,0 +1,21 @@
+// 2997. Minimum number of operations to make array XOR equal to K.
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int finalXor = 0;
+        for (int n : nums) {
+            finalXor = finalXor ^ n;
+        }
+        
+        int count = 0;
+        while (k || finalXor) {
+            if ((k % 2) != (finalXor % 2)) {
+                count++;
+            }
+            k /= 2;
+            finalXor /= 2;
+        }
+        
+        return count;
+    }
+};
