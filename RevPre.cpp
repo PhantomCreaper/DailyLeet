856ffe4273fe4e511e1d3f28ac534cb71cd5d05f
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-05-01 16:33:54 +0530

    Create RevPre.cpp

---
+++ RevPre.cpp
@@ -0,0 +1,11 @@
+// 2000. Reverse Prefix of Word
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+    int idx = word.find(ch);
+    if (idx != string::npos) {
+        reverse(word.begin(), word.begin() + idx + 1);
+    }
+    return word;
+}
+};
