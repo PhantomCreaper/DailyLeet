1e363ab38f228d46589e12b667ffbe378d279025
Arjunn Shetty <131944608+PhantomCreaper@users.noreply.github.com>
2024-06-17 10:07:50 +0530

    Create SumOfSquareNumbers.cpp

---
+++ SumOfSquareNumbers.cpp
@@ -0,0 +1,20 @@
+// 633. Sum of Square Numbers
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long left = 0;
+        long right = sqrt(c);
+
+        while(left <= right){
+            long sum = left * left + right * right;
+            if(sum == c){
+                return true;
+            } else if(sum < c){
+                left++;
+            } else{
+                right--;
+            }
+        }
+        return false;
+    }
+};
